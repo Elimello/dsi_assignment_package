2d8560aff01510ab3a6fd4be0c124dd9971a5e7f
Eli <88291925+Elimello@users.noreply.github.com>
2024-01-11 22:04:39 -0500

    Add files via upload

--- assignmentpkg/Analysis.py
+++ assignmentpkg/Analysis.py
@@ -27,6 +27,6 @@ class Analysis():
   def compute_analysis(self) -> Any:
     pass
 
-  def plot_data(self, save_path:Optional[str] = None) -> matplotlib.Figure:
+  def plot_data(self, save_path:Optional[str] = None)
     pass
 
