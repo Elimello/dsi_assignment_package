3030465c3d352d3c1771cf5a0a77f79113e3c1e2
Eli <88291925+Elimello@users.noreply.github.com>
2024-01-11 22:51:05 -0500

    Update Analysis

--- assignmentpkg/Analysis.py
+++ assignmentpkg/Analysis.py
@@ -70,7 +70,7 @@ class Analysis():
 
     pass
 
-  def get_user_data(self) -> dict:
+  def get_user_data(self) -> Any:
 
     """ Get the authenticated user data object from GitHub.
     Connect to the GitHub API and retrieve the authenticated users' data as Python dictionary.
